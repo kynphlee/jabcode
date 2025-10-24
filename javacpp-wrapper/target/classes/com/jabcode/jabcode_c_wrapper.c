ea24cdd8992b7fd71f3f4aba17a0fa398ccac8a2
kynphlee <kynphlee@gmail.com>
2025-10-24 01:24:55 -0400

    refactor: enhance JNI interface with additional experimental functions for classifier and pipeline debugging

--- javacpp-wrapper/target/classes/com/jabcode/jabcode_c_wrapper.c
+++ javacpp-wrapper/target/classes/com/jabcode/jabcode_c_wrapper.c
@@ -5,6 +5,16 @@
 
 #include "jabcode_c_wrapper.h"
 #include <stdio.h>
+#include <string.h>
+
+// Forward declaration to avoid including encoder.h here
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern void setDefaultPalette(jab_int32 color_number, jab_byte* palette);
+#ifdef __cplusplus
+}
+#endif
 
 jab_encode* createEncode_c(jab_int32 color_number, jab_int32 symbol_number) {
     return createEncode(color_number, symbol_number);
@@ -40,4 +50,20 @@ jab_bitmap* readImage_c(jab_char* filename) {
 
 void reportError_c(jab_char* message) {
     reportError(message);
+}
+
+void getEncoderDefaultPalette_c(jab_int32 color_number, jab_int32* out, jab_int32 len) {
+    if (!out || len <= 0) return;
+    if (color_number != 16 && color_number != 32 && color_number != 64 && color_number != 128 && color_number != 256 && color_number != 8 && color_number != 4) {
+        // Default to 8 if invalid
+        color_number = 8;
+    }
+    jab_int32 bytes = color_number * 3;
+    jab_byte buf[256 * 3];
+    memset(buf, 0, sizeof(buf));
+    setDefaultPalette(color_number, buf);
+    jab_int32 n = bytes;
+    if (n > len) n = len;
+    for (jab_int32 i = 0; i < n; ++i) out[i] = (jab_int32)buf[i];
+    for (jab_int32 i = n; i < len; ++i) out[i] = 0;
 }
